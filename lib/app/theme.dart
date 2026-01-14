0ca2eb22a02abffd7fc688a54ad3afbb4d278406
shulamitmorjossef <shulamitmorjossef@gmail.com>
2026-01-14 20:26:35 +0200

    Connected to DB

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -1,26 +1,3 @@
-// // lib/app/theme.dart
-// import 'package:flutter/material.dart';
-
-// class AppTheme {
-//   static final lightTheme = ThemeData(
-//     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//     useMaterial3: true,
-//     textTheme: const TextTheme(
-//       headline1: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-//       bodyText1: TextStyle(fontSize: 16),
-//     ),
-//   );
-
-//   static final darkTheme = ThemeData(
-//     brightness: Brightness.dark,
-//     colorScheme: ColorScheme.fromSeed(
-//       seedColor: Colors.deepPurple,
-//       brightness: Brightness.dark,
-//     ),
-//     useMaterial3: true,
-//   );
-// }
-
 import 'package:flutter/material.dart';
 
 class AppTheme {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:collecta/main.dart';
+// import 'package:collecta/main.dart';
+
+import 'package:collecta/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
