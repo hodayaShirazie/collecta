84a07c2322d6c40398e29f29ce62a9cca2fa5836
hodaya <hoddaya.sh2003@gmail.com>
2026-04-28 09:08:03 +0300

    fixed bug in adding and deleting products in report donation

--- lib/ui/utils/donation/donation_toggle_product_helper.dart
+++ lib/ui/utils/donation/donation_toggle_product_helper.dart
@@ -11,59 +11,52 @@ class DonationToggleProductHelper {
     required List<Map<String, dynamic>> donatedItems,
     required VoidCallback refresh,
   }) async {
-    final name = product["name"];
-    final id = product["id"];
+    final name = product["name"] as String;
+    final id = product["id"] as String?;
 
     if (selectedProducts.contains(name)) {
       selectedProducts.remove(name);
+      if (name != "אחר") {
+        donatedItems.removeWhere((item) => item["productTypeId"] == id);
+      } else {
+        final lastIndex = donatedItems.lastIndexWhere(
+          (item) => (item["name"] as String?)?.startsWith("אחר") ?? false,
+        );
+        if (lastIndex >= 0) donatedItems.removeAt(lastIndex);
+      }
       refresh();
       return;
     }
 
     selectedProducts.add(name);
 
-    // if (name == "אחר") {
-    //   final result = await showOtherItemDialog(context: context);
-
-    //   if (result != null) {
-    //     donatedItems.add(result);
-    //   }
-    // } else {
-    //   final result = await showQuantityDialog(
-    //     context: context,
-    //     productName: name,
-    //     productId: id,
-    //   );
-
-    //   if (result != null) {
-    //     donatedItems.add(result);
-    //   }
-    // }
     if (name == "אחר") {
-    final result = await showOtherItemDialog(context: context);
-
-    if (result != null) {
-      donatedItems.add({
-        ...result,
-        "id": "",
-        "productTypeId": null,
-      });
+      final result = await showOtherItemDialog(context: context);
+      if (result != null) {
+        donatedItems.add({
+          ...result,
+          "id": "",
+          "productTypeId": null,
+        });
+      } else {
+        selectedProducts.remove(name);
+      }
+    } else {
+      final result = await showQuantityDialog(
+        context: context,
+        productName: name,
+        productId: id,
+      );
+      if (result != null) {
+        donatedItems.add({
+          ...result,
+          "id": "",
+          "productTypeId": id,
+        });
+      } else {
+        selectedProducts.remove(name);
+      }
     }
-  } else {
-    final result = await showQuantityDialog(
-      context: context,
-      productName: name,
-      productId: id,
-    );
-
-    if (result != null) {
-      donatedItems.add({
-        ...result,
-        "id": "",
-        "productTypeId": id,
-      });
-    }
-  }
 
     refresh();
   }
